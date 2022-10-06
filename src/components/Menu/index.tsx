6eb897424d7e69f28f93e9924242c120c091e7e0
Marllon <marllon3636@gmail.com>
2022-10-06 10:47:05 -0300

    feat(src/components): create menu styled component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './styles';
+
+const Menu: React.FC = () => (
+  <Container>
+    <nav>
+      <a href="/">Início</a>
+      <a href="/CategoriaCliente">Categoria Cliente</a>
+    </nav>
+  </Container>
+)
+
+export default Menu;
---
+++ src/components/Menu/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border-radius: 6px;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.1);
+
+  a {
+    margin: 5px;
+    text-decoration: none;
+  }
+`
