1bb00b73aa913008755041f4ff2034df34a4d342
Marllon <marllon3636@gmail.com>
2022-10-07 02:57:04 -0300

    feat(src/components): add delete options to table, adjust table

--- src/components/CategoryList/index.tsx
+++ src/components/CategoryList/index.tsx
@@ -1,35 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from './styles';
+import React, { useCallback, useEffect, useState } from 'react'
+import ClipLoader from "react-spinners/ClipLoader"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { IoTrashOutline } from "react-icons/io5"
+import { Category } from '../../interfaces/ICategory';
+import { Container, LoaderContainer, Table, ButtonArea } from './styles';
 
 const CategoryList: React.FC = () => {
-  const [categoriesList, setCategoriesList] = useState<any[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const categories = async () =>
-    setLoading(true);
-  fetch('https://cnctesteapl.azurewebsites.net/odata/CategoriaCliente?$select=nome,descricao&$Filter=IdEntidadeSindical/Id eq 6a8be2a2-2636-43d4-b9c0-002a50888604', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json;odata=verbose',
-    },
-  })
-    .then((response) => response.json())
-    .then((res) => {
-      setLoading(false);
-      setCategoriesList(res.value);
-    });
+
+  const loadCategories = useCallback(async () => {
+    try {
+      setLoading(true);
+      fetch('https://cnctesteapl.azurewebsites.net/odata/CategoriaCliente?$select=id,nome,descricao&$Filter=IdEntidadeSindical/Id eq 6a8be2a2-2636-43d4-b9c0-002a50888604', {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          setLoading(false);
+          setCategories(res.value);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [setCategories, setLoading]);
+
+  const deleteCategory = async (id: string) => {
+    const test = toast.loading("Please wait...");
+    try {
+      fetch(`https://cnctesteapl.azurewebsites.net/odata/CategoriaCliente(${id})`, {
+        method: 'DELETE',
+      })
+        .then((response) => {
+          loadCategories()
+          if (response.status === 200) {
+            toast.update(
+              test,
+              {
+                render: "Categoria excluída com sucesso!",
+                type: "success",
+                closeButton: null,
+                autoClose: 5000,
+                isLoading: false
+              }
+            )
+          }
+          if (response.status === 500) {
+            toast.update(
+              test,
+              {
+                render: "Algo deu errado",
+                type: "error",
+                closeButton: null,
+                autoClose: 5000,
+                isLoading: false
+              }
+            );
+          }
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleDelete = async (id: any) => {
+    await deleteCategory(id);
+  }
 
   useEffect(() => {
-    categories();
-  }, []);
+    loadCategories();
+  }, [loadCategories]);
 
   return (
     <>
       <Container>
-        {!loading ? categoriesList?.map(category => {
-          return <p key={Math.random()}>{category.Nome} {category.Descricao}</p>
-        }) : <p>Carregando...</p>}
+        <header>Listagem de Categorias</header>
+        {loading ?
+          <LoaderContainer>
+            <ClipLoader color={"orange"} />
+          </LoaderContainer>
+          :
+          <Table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Excluir</th>
+              </tr>
+            </thead>
+            <tbody>
+              {categories.map((category: Category) => {
+                return (
+                  <tr key={category.Id}>
+                    <td>{category.Nome}</td>
+                    <td>{category.Descricao ? category.Descricao : "Não informado."}</td>
+                    <td>
+                      <ButtonArea>
+                        <button onClick={() => handleDelete(category.Id)}><IoTrashOutline /></button>
+                      </ButtonArea>
+                    </td>
+                  </tr>)
+              })}
+            </tbody>
+          </Table>
+        }
       </Container>
+      <ToastContainer />
     </>
   )
 }
--- src/components/CategoryList/styles.tsx
+++ src/components/CategoryList/styles.tsx
@@ -1,41 +1,88 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
   border-radius: 6px;
-  margin: 30px 20px;
+  margin: 15px 20px 0px;
   padding: 33px;
   background-color: #fff;
   box-shadow: 0 5px 10px rgba(0,0,0,0.1);
+
+  header {
+    width: 100%;
+    position: relative;
+    white-space: nowrap;
+    font-size: 20px;
+    font-weight: 600;
+    color: #0E1207;
+  }
+
+  header::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -5px;
+    height: 3px;
+    width: 27px;
+    border-radius: 8px;
+    background-color: orange;
+  }
 `
 
-const SpinAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
+export const LoaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 60px 60px 30px;
+
+  .span {
   }
 `
-export const LoaderContainer = styled.div`
-  width: 100%;
-  height: 100vh;
+
+export const Table = styled.table`
+  margin-top: 30px;
+  table-layout: fixed;
+
+  th {
+    color: #0E1207;
+    white-space: nowrap;
+  }
+
+  td, th {
+    text-align: left;
+    padding: 10px;
+    white-space: pre-wrap;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+`
+
+export const ButtonArea = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  position: fixed;
-  background: rgba(0, 0, 0, 0.834);
-  z-index: 1;
 
-  .spinner {
-    width: 64px;
-    height: 64px;
-    border: 8px solid;
-    border-color: #3d5af1 transparent #3d5af1 transparent;
-    border-radius: 50%;
-    animation: SpinAnimation 1.2s linear infinite;
-    animation-name: ${SpinAnimation};
-    animation-duration: 8s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-}
+  button {
+    align-self: center;
+    border: none;
+    height: 40px;
+    font-weight: 900;
+    cursor: pointer;
+    margin: 5px;
+    background-color: transparent;
+    color: white;
+    text-transform: uppercase;
+    &:hover {
+      svg {
+      color: #dd373a;
+    }
+    }
+  }
+
+  svg {
+    height: 29px;
+    width: 30px;
+    color: black;
+    transition: 0.25s;
+  }
 `
